a0899036791be36886cc4e76ee73d5624abdfd92
artemiusgreat <arteinvolo@gmail.com>
2022-04-03 13:10:50 -0400

    Fix package

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Can be used with any Blazor app, either Server side or Wasm.
 # Nuget
 
 ```
-Install-Package ScriptContainer -Version 1.0.6-prerelease
+Install-Package ScriptContainer -Version 1.0.7-prerelease
 ```
 
 # Sample 
--- Samples/SampleServer/Pages/Error.cshtml.cs
+++ Samples/SampleServer/Pages/Error.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
@@ -8,7 +8,7 @@ namespace SampleServer.Pages
   [IgnoreAntiforgeryToken]
   public class ErrorModel : PageModel
   {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
